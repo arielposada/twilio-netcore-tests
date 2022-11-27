167b22e776a4cd966efa61b3c8746f0370454bbb
arielposada <116775523+arielposada@users.noreply.github.com>
2022-11-26 21:41:37 -0600

    First commit

---
+++ twilio-netcore-tests/twilio-netcore-tests/Program.cs
@@ -0,0 +1,10 @@
+using twilio_netcore_tests;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
