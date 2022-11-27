5591296241a65f48dbdcd133e63f476a84a12f8d
arielposada <116775523+arielposada@users.noreply.github.com>
2022-11-26 22:27:41 -0600

    Create TwilioClientService.cs

---
+++ twilio-netcore-tests/logic/TwilioClientService.cs
@@ -0,0 +1,41 @@
+﻿using core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+
+namespace logic
+{
+    /// <summary>
+    /// Twilio client
+    /// </summary>
+    public class TwilioClientService : IDisposable
+    {
+        public TwilioClientService(string accountSid, string authToken)
+        {
+            TwilioClient.Init(accountSid, authToken);
+        }
+
+        /// <summary>
+        /// Send a message with Twilio
+        /// </summary>
+        /// <param name="messageRequest">Message request</param>
+        /// <returns>Message Id as String</returns>
+        public async Task<string> SendMessage(WhatsappMessageRequest messageRequest) 
+        {
+            //Taken from the example in: https://www.twilio.com/docs/libraries/csharp-dotnet/details
+            var message = await MessageResource.CreateAsync(
+                body: messageRequest.body,
+                from: new Twilio.Types.PhoneNumber(messageRequest.from),
+                to: new Twilio.Types.PhoneNumber(messageRequest.to)
+            );
+
+            return message.Sid;
+        }
+
+        public void Dispose() { }
+    }
+}
